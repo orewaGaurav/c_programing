7bfda3e104fd9a0e40f6bc1c39996a2e366ce125
Gaurav Kumar <gauravkumar47967@gmail.com>
2024-02-21 20:46:24 +0530

    call by reference

---
+++ swap.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+//call by reference
+
+void swap(int a,int b);
+void _swap(int *a, int *b);
+
+int main(void){
+    int x = 3;
+    // printf("enter x = ");
+    // scanf("%p",&x);
+    int y = 5;
+    // printf("enter y = ");
+    // scanf("%p",y);
+    _swap(&x , &y);
+    printf("x = %d & y = %d\n",x,y);
+    return 0;
+}
+//call by reference
+void _swap(int *a,int *b){
+    int t = *a;
+    *a = *b;
+    *b = t;
+} 
