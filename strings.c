ff04c40ef5c5aaf298998a33ccd9452ac580b58f
Gaurav Kumar <gauravkumar47967@gmail.com>
2024-02-23 11:25:27 +0530

    strins bascics

---
+++ strings.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+//methods to print string
+int main(void){
+    char name[] = {'G','a','u','r','a','v','\0'};
+    printf("%s\n",name);
+    char class[] = "cyber security";
+    printf("%s\n",class);
+    
+
+    return 0;
+
+}
