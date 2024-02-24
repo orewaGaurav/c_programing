e16568b67649e2e580f8391f57e85a789e95b9ff
Gaurav Kumar <gauravkumar47967@gmail.com>
2024-02-24 11:02:26 +0530

    strlen

---
+++ strings6.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<string.h>
+int main(void){
+    // //1st method
+    // char len[] = "Gaurav";
+    // int length = strlen(len);
+    // printf("%d\n",length);
+
+
+    //2nd way
+
+    // char len[] = "Gaurav";
+    // //special type of declaration that is "%lu"
+    // printf("%lu\n",strlen(len));
+
+
+    //3rd way
+    char len[100];
+    fgets(len,100,stdin);
+    int length = strlen(len);
+    int finallength = length - 1;
+    printf("%d\n",finallength);
+    return 0;
+
+
+
+
+}
