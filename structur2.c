5b3a626f4a958d9f8a51d8689e703ee04e3be91e
Gaurav Kumar <gauravkumar47967@gmail.com>
2024-03-05 23:57:03 +0530

    structure

--- structur2.c
+++ structur2.c
@@ -18,4 +18,13 @@ int main(void){
     printf("name of the product: %s\n",p1.name);
     printf("price of the product: %f\n",p1.price);
     printf("gst on the product: %d\n",p1.gst);
+
+    struct product p2;
+    p2.price = 6000;
+    p2.gst = 25;
+    strcpy(p2.name,"Rolex watch");
+    printf("name of the product: %s\n",p2.name);
+    printf("price of the product: %f\n",p2.price);
+    printf("gst on the product: %d\n",p2.gst);
+
 }
